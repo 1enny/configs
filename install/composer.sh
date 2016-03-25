20008243c1f6ce6ae8bc57e26a3f44a6e9d2a2fe
pashkevich <sergey.pashkevich@uvoteam.com>
2016-03-25 06:55:40 +0200

    improvement

--- install/composer.sh
+++ install/composer.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-echo ">>> Installing Composer"
+if which composer >/dev/null; then
+    echo ">>> Installing Composer"
 
-# Composer
-curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
+    # Composer
+    curl -sS https://getcomposer.org/installer | php
+    sudo mv composer.phar /usr/local/bin/composer
+    echo "PATH=$PATH:/usr/local/bin/" > ~/.profile
+else
+    echo ">>> Composer exists in system!"
+fi;
